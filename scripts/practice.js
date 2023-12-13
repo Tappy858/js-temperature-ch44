5461caf7edb53f7542f998ca757b9eb1aa3ce382
darius <darius_taplin@yahoo.com>
2023-12-12 19:38:05 -0800

    variables

---
+++ scripts/practice.js
@@ -0,0 +1,40 @@
+console.log("practice");
+
+//student info
+let studentName='Darius';
+let myGrade=4.20;
+let isStudent=true;
+
+//prfessor info
+let professorNamme="sam"
+let herGrade="4.0"
+let isProfessor = true;
+
+//display values
+console.log(studentName);
+console.log(myGrade);
+console.log(isStudent);
+
+console.log("My name is " + studentName);
+console.log("FSDI 101 grade: " + myGrade);
+console.log("Active: " + isStudent);
+
+let num1=10;
+let num2=5;
+
+console.log("Muliplication: " + num1*num2);
+
+let assignmentName = "Class 1 - Assingment 1";
+let description= "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+let pointsAs1 = 6.66;
+let pointsAs2 = 6.66;
+let pointsAs3 = 6.66;
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads= true;
+let studentAnnotation = false;
+
+let dueDate="12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);
