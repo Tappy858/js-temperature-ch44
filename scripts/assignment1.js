ecfe920fd1db7f5aa5dab88a61aef2738ba3c4ad
darius <darius_taplin@yahoo.com>
2023-12-14 11:07:30 -0800

    class1

---
+++ scripts/assignment1.js
@@ -0,0 +1,31 @@
+let movieTitle="Ozark";
+let movieMatch=95 +'%' + ' match';
+let movieReleasedate=2017;
+let movieRating="TV-MA";
+let movieSeason=4 + ' Seasons';
+let movieQuality="HD"
+let movieAudiio="5.1"
+let movieEpisode='S' + 4 + ":" + 'E' + 1;
+let movieEpisodetitle="The beginning of the End";
+let movieInfo="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, odio laboriosam dolor officiis necessitatibus delectus blanditiis atque, ratione aspernatur reiciendis illum? Molestiae consequatur ea vitae quos debitis. Culpa, obcaecati sunt.";
+let movieCast="Cast: Jason Bateman, Laura Linney, Sofia Hublitz";
+let movieCreators="Creators: Bill Dubuque, Mark Williams";
+let myList="My List"
+let movieRate="Rate"
+let seasonSelect="SEASON 4";
+
+document.write(`<h1> ${movieTitle} </h1>`);
+document.write(`<p> ${movieMatch} </p>`);
+document.write(`<p> ${movieReleasedate} </p>`);
+document.write(`<p> ${movieRating} </p>`);
+document.write(`<p> ${movieSeason} </p>`);
+document.write(`<p> ${movieQuality} </p>`);
+document.write(`<p> ${movieAudiio} </p>`);
+document.write(`<h3> ${movieEpisode} </h3>`);
+document.write(`<h3> ${movieEpisodetitle} </h3>`);
+document.write(`<p> ${movieInfo} </p>`);
+document.write(`<p> ${movieCast} </p>`);
+document.write(`<p> ${movieCreators} </p>`);
+document.write(`<p> ${myList} </p>`);
+document.write(`<p> ${movieRate} </p>`);
+document.write(`<h2> ${seasonSelect} </h2>`);
